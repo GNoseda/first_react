15fb65308e5dc45e07f69b16592fd821e4cc25d6
GNoseda <g.noseda.soto@gmail.com>
2023-01-20 12:52:01 -0300

    first React practice app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+const Message = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p style={{color: props.color}}>My name is {props.name}, and i'm {props.age} years old</p>
     </div>
-  );
+  )
+}
+
+const Footer = (author) => {
+  return (
+    <div>
+      greeting app created by {author.name}
+    </div>
+  )
+}
+
+const App = () => {
+  const now = new Date()
+  const a = 11
+  const b = 20
+  console.log(now, a+b)
+  console.log("Hello world from console")
+  return (
+    <div> 
+      <p> Hello World </p> 
+      <p> Current date is: {now.toString()} </p>
+      <p> {a} plus {b} equals {a+b} </p>  
+      <Message name="Gianca" age="32" color="red" />
+      <Message name="Pauly" age={a+b+1} color="blue"/>
+      <Message name="Diego" age={a+b} color="green"/>
+      <Footer name="Giancarlo Noseda"/>
+    </div>
+  )
 }
 
 export default App;
